f3acdcd0dd884f8b2126a16d9327693280d3e30a
pedrobgodoy <pedrohenrique.godoy@Outlook.com>
2019-03-29 23:07:18 -0300

    Now render function is in loop, open zeros changed

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const mapSlotsArray = [];
 const showSlotArray = [];
+const markedSlotArray = [];
 let xSize = 15;
 let ySize = 15;
-let bombs = 50;
+let bombs = 20;
+let renderDifference = true;
 
 function start() {
   createMap();
@@ -17,10 +19,12 @@ function createMap() {
   for (let i = 0; i < numberOfSlots; i++) {
     mapSlotsArray[i] = 0;
     showSlotArray[i] = false;
+    markedSlotArray[i] = false;
   }
 
   mapSlotsArray[numberOfSlots] = 0;
   showSlotArray[numberOfSlots] = false;
+  markedSlotArray[numberOfSlots] = false;
 }
 
 function placeBombsRandomAndAddToSlots() {
@@ -34,17 +38,11 @@ function placeBombsRandomAndAddToSlots() {
       i--;
     }
   }
+  renderDifference = true;
 }
 
 function addToSides(slotIndex) {
-  addToSlot(slotIndex - (xSize + 1));
-  addToSlot(slotIndex - xSize);
-  addToSlot(slotIndex - (xSize - 1));
-  addToSlot(slotIndex - 1);
-  addToSlot(slotIndex + 1);
-  addToSlot(slotIndex + (xSize - 1));
-  addToSlot(slotIndex + xSize);
-  addToSlot(slotIndex + (xSize + 1));
+  doSomethingToAllSides(slotIndex, addToSlot);
 }
 
 function addToSlot(slotIndex) {
@@ -58,73 +56,129 @@ function addToSlot(slotIndex) {
 }
 
 function renderSlots() {
-  let html = "<table class='slotsCanvas' cellpadding=0 cellspacing=0>";
+  if (renderDifference) {
+    renderDifference = false;
 
-  for (let row = 0; row < ySize; row++) {
-    html += "<tr>";
+    let html = "<table class='slotsCanvas' cellpadding=0 cellspacing=0>";
 
-    for (let column = 0; column < xSize; column++) {
-      const slotIndex = column + ySize * row;
-      const slotValue = mapSlotsArray[slotIndex];
-      let showValue = slotValue;
-      if(slotValue === 9){
-        showValue = "X";
+    for (let row = 0; row < ySize; row++) {
+      html += "<tr>";
+
+      for (let column = 0; column < xSize; column++) {
+        const slotIndex = column + ySize * row;
+        const slotValue = mapSlotsArray[slotIndex];
+        let showValue = slotValue;
+        if (slotValue === 9) {
+          showValue = "#";
+        }
+
+        html +=
+          "<td class='slot' oncontextmenu='rightClick(\"" +
+          slotIndex +
+          "\"); return false;'>";
+
+        if (markedSlotArray[slotIndex]) {
+          html += `<button type='button' class='btn-slot-marked' onclick=""></button>`;
+        } else {
+          html += showSlotArray[slotIndex]
+            ? `<button type='button' class='btn-slot-shown' onclick="clickSlot(${slotIndex});">${showValue}</button>`
+            : `<button type='button' class='btn-slot' onclick="clickSlot(${slotIndex});"></button>`;
+        }
+
+        html += "</td>";
       }
 
-      html += "<td class='slot'>";
-      html += showSlotArray[slotIndex]
-        ? `<button type='button' class='btn-slot-shown' onclick="clickSlot(${slotIndex});">${showValue}</button>`
-        : `<button type='button' class='btn-slot' onclick="clickSlot(${slotIndex});"></button>`;
-      html += "</td>";
+      html += "</tr>";
     }
 
-    html += "</tr>";
+    html += "</table>";
+
+    document.querySelector("#slotCanvas").innerHTML = html;
   }
 
-  html += "</table>";
+  setTimeout(() => {
+    renderSlots();
+  }, 10);
+}
 
-  document.querySelector("#slotCanvas").innerHTML = html;
+function rightClick(slotIndex) {
+  if (!showSlotArray[slotIndex]) {
+    if (markedSlotArray[slotIndex]) {
+      markedSlotArray[slotIndex] = false;
+      renderDifference = true;
+    } else {
+      markedSlotArray[slotIndex] = true;
+      renderDifference = true;
+    }
+  }
 }
 
 function clickSlot(slotIndex) {
   showSlotArray[slotIndex] = true;
-  renderSlots();
   const slotValue = mapSlotsArray[slotIndex];
+  renderDifference = true;
 
   if (slotValue === 9) {
     endGame();
     return;
   }
 
-  if(slotValue === 0){
-    //checkZeroSlotsRepercursivily(slotIndex);
-
-    renderSlots();
+  if (checkIfZeroAndClosed(slotIndex)) {
+    openSides(slotIndex);
+    checkSidesAndOpenZeros(slotIndex);
   }
 }
 
-function endGame(){
-  for(let i=0;i<(xSize * ySize);i++){
+function checkIfZeroAndClosed(slotIndex) {
+  if (mapSlotsArray[slotIndex] === 0 && showSlotArray[slotIndex]) {
+    return true;
+  }
+
+  return false;
+}
+
+function openSides(slotIndex) {
+  doSomethingToAllSides(slotIndex, openSlot);
+}
+
+function checkSidesAndOpenZeros(slotIndex) {
+  doSomethingToAllSides(slotIndex, checkSideAndOpen);
+}
+
+function checkSideAndOpen(slotIndex) {
+  if (!checkIfZeroAndClosed(slotIndex)) return;
+
+  openSides(slotIndex);
+  checkSidesAndOpenZeros(slotIndex);
+}
+
+function openSlot(slotIndex) {
+  showSlotArray[slotIndex] = true;
+  renderDifference = true;
+}
+
+function endGame() {
+  for (let i = 0; i < xSize * ySize; i++) {
     showSlotArray[i] = true;
   }
-  renderSlots();
+  renderDifference = true;
 }
 
-function checkZeroSlotsRepercursivily(slotIndex){
-  // (n - xSize) TOP
-  // (n - 1)     LEFT
-  // (n + 1)     RIGHT
-  // (n + xSize) BOTTOM
-  checkZeroSlotAndChangeVisibilityRep(slotIndex-xSize);
-  checkZeroSlotAndChangeVisibilityRep(slotIndex-1);
-  checkZeroSlotAndChangeVisibilityRep(slotIndex+1);
-  checkZeroSlotAndChangeVisibilityRep(slotIndex+xSize);
+/// UTILITY
+function doSomethingToDirectSides(slotIndex, action) {
+  action(slotIndex - xSize);
+  action(slotIndex - 1);
+  action(slotIndex + 1);
+  action(slotIndex + xSize);
 }
 
-function checkZeroSlotAndChangeVisibilityRep(slotIndex){
-  if(mapSlotsArray[slotIndex] === 0)
-  {
-    showSlotArray[slotIndex] = true;
-    checkZeroSlotsRepercursivily(slotIndex);
-  }
+function doSomethingToAllSides(slotIndex, action) {
+  action(slotIndex - xSize - 1);
+  action(slotIndex - xSize);
+  action(slotIndex - xSize + 1);
+  action(slotIndex - 1);
+  action(slotIndex + 1);
+  action(slotIndex + xSize - 1);
+  action(slotIndex + xSize);
+  action(slotIndex + xSize + 1);
 }
